88a16d0a5b822c10cd93cb1276e6d063c4a25874
Evan Evers <evan.evers@biola.edu>
2022-11-15 23:34:27 -0800

    Hi-Fi Prototype

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.todolist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //move to messageDemo immediately
+        Intent intent = new Intent(this, createTask.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/todolist/taskList.java
+++ app/src/main/java/com/example/todolist/taskList.java
@@ -8,16 +8,16 @@ import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 
-public class Folders extends AppCompatActivity {
+public class taskList extends AppCompatActivity {
 
     Button trashButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_folders);
+        setContentView(R.layout.activity_task_list);
 
-        trashButton = findViewById(R.id.folder_trash);
+        trashButton = findViewById(R.id.task_trash);
 
         trashButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,28 +28,37 @@ public class Folders extends AppCompatActivity {
     }
 
     void showDeleteDialog() {
-        final Dialog dialog = new Dialog(Folders.this);
+        final Dialog dialog = new Dialog(taskList.this);
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);   //disable default title
         dialog.setCancelable(true); //user can disable dialog by clicking outside of it.
-        dialog.setContentView(R.layout.delete_folder_dialog);
+        dialog.setContentView(R.layout.delete_task_dialog);
 
         //Initialize dialog views.
-        Button yesButton = dialog.findViewById(R.id.yes_button);
-        Button noButton = dialog.findViewById(R.id.no_button);
+        Button deleteOneButton = dialog.findViewById(R.id.delete_one_button);
+        Button deleteAllButton = dialog.findViewById(R.id.delete_all_button);
+        Button cancelButton = dialog.findViewById(R.id.cancel_button);
 
         //on clicking the yes button, this onClick function will be called
-        yesButton.setOnClickListener(new View.OnClickListener() {
+        deleteOneButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                dialog.dismiss();   //get rid of the dialog. (functionality coming later)
+                dialog.dismiss();   //(functionality coming later)
             }
         });
 
         //on clicking the no button, this onClick function will be called
-        noButton.setOnClickListener(new View.OnClickListener() {
+        deleteAllButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                dialog.dismiss();   //get rid of the dialog. (functionality coming later)
+                dialog.dismiss();   //(functionality coming later)
+            }
+        });
+
+        //on clicking the no button, this onClick function will be called
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();   //get rid of the dialog.
             }
         });
 
